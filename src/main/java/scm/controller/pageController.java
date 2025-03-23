8ae27888c690d5615d56577fc18a5040777f37b6
ankitsubhamjyoti2005 <ankitsubhamjyoti2005@users.noreply.github.com>
2025-03-23 11:26:25 +0530

    modified:   src/main/java/scm/controller/pageController.java

--- src/main/java/scm/controller/pageController.java
+++ src/main/java/scm/controller/pageController.java
@@ -1,19 +1,57 @@
 package scm.controller;
 
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class pageController {
-    @RequestMapping("/")
-    public String home(Model model){
-        System.out.println("home page handler");
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/home";
+    }
+
+    @RequestMapping("/home")
+    public String home(Model model) {
+        System.out.println("Home page handler");
         // sending data to view
         model.addAttribute("name", "Substring Technologies");
         model.addAttribute("youtubeChannel", "Learn Code With Durgesh");
         model.addAttribute("githubRepo", "https://github.com/learncodewithdurgesh/");
         return "home";
+    }
+
+    // about route
+
+    @RequestMapping("/about")
+    public String aboutPage(Model model) {
+        model.addAttribute("isLogin", true);
+        System.out.println("About page loading");
+        return "about";
+    }
+    // this is showing login page
+    @GetMapping("/login")
+    public String login() {
+        return new String("login");
+    }
+
+    // registration page
+    @GetMapping("/register")
+    public String register(Model model) {
+        return "register";
+    }
+
+    // processing register
+
+    @RequestMapping(value = "/do-register", method = RequestMethod.POST)
+    public String processRegister(@Valid @ModelAttribute UserForm userForm, BindingResult rBindingResult, HttpSession session) {
+
     }
 }
